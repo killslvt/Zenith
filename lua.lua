584692c293c233a8012811e081e686986fc223f1
Isolxte <gageleeray2006@gmail.com>
2024-05-15 07:45:33 -0400

    Created My Own API Based Off Of Celery

---
+++ lua.lua
@@ -0,0 +1,14 @@
+local userInputService = game:GetService("UserInputService")
+
+local player = game.Players.localPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoid = character:WaitForChild("Humanoid")
+
+local function InfJump(input, gameProcess)
+    if gameProcess then return end
+    if input.KeyCode == Enum.KeyCode.Space then
+        humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+    end
+end
+
+userInputService.InputBegan:Connect(Fly)
