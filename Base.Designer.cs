ec301a53645b9e431225df7a03601d9e18af3106
Isolxte <gageleeray2006@gmail.com>
2024-05-24 14:35:00 -0400

    Idk

--- Base.Designer.cs
+++ Base.Designer.cs
@@ -80,7 +80,7 @@
             this.fastColoredTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.fastColoredTextBox1.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
             this.fastColoredTextBox1.Font = new System.Drawing.Font("Courier New", 9.75F);
-            this.fastColoredTextBox1.ForeColor = System.Drawing.Color.White;
+            this.fastColoredTextBox1.ForeColor = System.Drawing.Color.DimGray;
             this.fastColoredTextBox1.IndentBackColor = System.Drawing.Color.Transparent;
             this.fastColoredTextBox1.IsReplaceMode = false;
             this.fastColoredTextBox1.Language = FastColoredTextBoxNS.Language.Lua;
